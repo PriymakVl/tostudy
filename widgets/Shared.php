2e4cc4bd7f9701f47ba26ab514cb74dae6c9ed07
Vladimir <PriymakVl@mail.ru>
2020-12-16 06:43:04 +0300

    add telegram icons shared news page

--- widgets/Shared.php
+++ widgets/Shared.php
@@ -9,7 +9,7 @@ class Shared extends Widget
 
 	public function run()
 	{
-		$icons = '<div class="uSocial-Share" data-pid="60be2d312cd0ed983674dbcc610e9631" data-type="share" data-options="rect,style1,default,absolute,horizontal,size24,eachCounter1,counter0" data-social="fb,ok,vk,pinterest,twi" data-mobile="sms"></div>';
+		$icons = '<div class="uSocial-Share" data-pid="befe754ecf170b3d3399e1d6f5811c9b" data-type="share" data-options="rect,style1,default,absolute,horizontal,size24,eachCounter1,counter0" data-social="fb,ok,vk,pinterest,twi,telegram" data-mobile="sms"></div>';
 		
 		return $icons;
 	}
